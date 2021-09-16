29895ef65cec4ed46a3b75c651e5269f70f2da9f
Yuxing Fei <36567193+idocx@users.noreply.github.com>
2021-09-16 09:21:21 -0700

    feature(setup): add setup.py for installation

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='alab_control',
+    packages=find_packages(exclude="tests"),
+    version='0.1.0',
+    author='Alab Project Team',
+    python_requires=">3.6",
+    description='Drivers for alab devices',
+    zip_safe=False,
+    install_requires=[
+        "pyModbusTCP"
+    ],
+    include_package_data=True
+)
