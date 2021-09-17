c1203379321f569ca9dff820545584b44ee87cb5
Yuxing Fei <36567193+idocx@users.noreply.github.com>
2021-09-17 00:23:10 -0700

    test(ur_robot): add more test cases for robot arm

--- tests/test_robot_arm_ur5e.py
+++ tests/test_robot_arm_ur5e.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 from alab_control import robot_arm_ur5e
 
@@ -7,8 +8,19 @@ class TestUR5e(unittest.TestCase):
         self.robot = robot_arm_ur5e.URRobot(ip="192.168.182.135")
 
     def tearDown(self):
+        self.robot.stop()
         self.robot.close()  # close the connection
 
     def test_is_running(self):
         is_running = self.robot.is_running()
         self.assertFalse(is_running, msg="The robot should not be running now.")
+
+    def test_run_program(self):
+        self.robot.run_program("send_to_furnace_test")
+        time.sleep(1)
+        self.assertTrue(self.robot.is_running(), msg="The program does not start successfully.")
+        self.robot.pause()
+        time.sleep(1)
+        self.robot.continue_play()
+        self.assertFalse(self.robot.is_running(), msg="The program has not stop.")
+        self.assertEqual(self.robot.loaded_program, "send_to_furnace_test", msg="Inconsistent loading program.")
