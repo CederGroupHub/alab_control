5e37607b3115096a71c7fbc69cf126eb02bdcbef
rekumar <rekumar@lbl.gov>
2022-08-25 15:09:36 -0700

    draft 2

--- alab_control/labman/error.py
+++ alab_control/labman/error.py
@@ -10,5 +10,13 @@ class WorkflowError(LabmanError):
     """A generic error for Labman workflows"""
 
 
+class WorkflowFullError(WorkflowError):
+    """Error indicating a workflow is either:
+    - completely full
+    - does not have enough remaining capacity to include a requested InputFile
+    - has just been closed to send to the Labman
+    """
+
+
 class LabmanCommunicationError(LabmanError):
     """Error communicating with Labman"""
