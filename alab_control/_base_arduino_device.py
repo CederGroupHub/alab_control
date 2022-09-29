f389e495067e79cac0625e86bb8bf8d79cf8f666
Yuxing Fei <yuxingfei@berkeley.edu>
2022-09-29 14:59:45 -0700

    feat(shaker): driver code for shaker

---
+++ alab_control/_base_arduino_device.py
@@ -0,0 +1,26 @@
+"""
+A base device abstraction for all the aduino devices that have RESTful API endpoints
+"""
+import abc
+import time
+from typing import Dict, Union, Optional
+
+import requests
+
+
+class BaseArduinoDevice(abc.ABC):
+    def __init__(self, ip_address, port: int = 80):
+        self.ip_address = ip_address
+        self.port = port
+
+    @abc.abstractmethod
+    def send_request(self, endpoint: str, data: Optional[Dict[str, Union[str, int, float, bytes, bool]]] = None,
+                     method: str = "GET", jsonify: bool = True, suppress_error: bool = False):
+        url = f"http://{self.ip_address}:{self.port}/{endpoint}"
+        time.sleep(0.1)
+        response = requests.request(method=method, url=url, data=data)
+        if not suppress_error:
+            response.raise_for_status()
+        if jsonify:
+            return response.json()
+        return response
