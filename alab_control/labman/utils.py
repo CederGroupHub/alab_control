89c686e9a2b2386d39bddb3247a8c65b29a6888a
rekumar <rekumar@lbl.gov>
2023-01-10 11:36:25 -0800

    logging in init

--- alab_control/labman/utils.py
+++ alab_control/labman/utils.py
@@ -1,8 +1,8 @@
-from .database import PowderView, CrucibleView, JarView, InputFileView
+from .database import PowderView, CrucibleView, JarView, InputFileView, LoggingView
 from .database.data_objects import db_exists
 
 
 def initialize_labman_database(overwrite_existing=False):
     if overwrite_existing or not db_exists():
-        for view in [PowderView, CrucibleView, JarView, InputFileView]:
+        for view in [PowderView, CrucibleView, JarView, InputFileView, LoggingView]:
             view()._initialize()
