50cd5ebbcf15c9420692d04d91d59cb57c96ece0
rekumar <rekumar@lbl.gov>
2023-01-10 12:39:35 -0800

    api abstract

--- alab_control/labman/api.py
+++ alab_control/labman/api.py
@@ -3,12 +3,16 @@ from typing import Literal
 
 
 class LabmanAPI:
+    DEFAULTS = {
+        "timeout": 5,
+    }
+
     def __init__(self, url: str, port: int):
         self.API_BASE = f"{url}:{port}"
 
     def get_status(self):
         url = f"{self.API_BASE}/GetStatus"
-        return requests.get(url=url)
+        return requests.get(url=url, **self.DEFAULTS)
 
     def request_indexing_rack_control(self, index: Literal[1, 2, 3, 4]):
         url = f"{self.API_BASE}/RequestIndexingRackControl"
