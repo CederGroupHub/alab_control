0c214565c3c5f4e27d02b0cdfb0a32a54f893da8
Bernardus Rendy <37468936+bernardusrendy@users.noreply.github.com>
2023-02-07 16:33:51 -0800

    Door_Controller Calibration

--- alab_control/door_controller/door_controller.py
+++ alab_control/door_controller/door_controller.py
@@ -27,7 +27,7 @@ class DoorController(BaseArduinoDevice):
             try:
                 clientSocket.connect((self.ip_address,self.port))
             except:
-                print( "re-connecting" )  
+                # print( "re-connecting" )  
                 connected=False
                 retry=0
                 while not connected and retry <= max_retries:
@@ -35,7 +35,7 @@ class DoorController(BaseArduinoDevice):
                         retry+=1
                         clientSocket.connect((self.ip_address,self.port))
                         connected = True
-                        print( "re-connection successful" )  
+                        # print( "re-connection successful" )  
                     except socket.error:  
                         time.sleep(1)
             # Send data to server
