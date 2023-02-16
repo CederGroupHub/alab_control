5fe5f6b1d9079df43997eaf85dfc28c1dabd95be
Bernardus Rendy <37468936+bernardusrendy@users.noreply.github.com>
2023-02-16 11:04:49 -0800

    Arduino programs

---
+++ arduino_programs/ball_dispenser/driver5mm/driver5mm.ino
@@ -0,0 +1,417 @@
+// This is a demo of the RBBB running as webserver with the EtherCard
+// 2010-05-28 <jc@wippler.nl>
+//
+// License: GPLv2
+
+#include <Arduino.h>
+#include <EtherCard.h>
+#include<ArduinoJson.h>
+#include <AceRoutine.h>
+
+#include <Wire.h>
+#include <Stepper.h>
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiWire.h"
+
+using namespace ace_routine;
+
+//#define IRPin1 2 // Have to be interrupt pin
+//#define IRPin2 3 // Have to be interrupt pin
+
+#define stepPin1 4
+#define stepPin2 6
+#define stepPin3 5
+#define stepPin4 7
+
+#define button1 A0
+#define button2 A1
+#define button3 A2
+
+#define STEPS_PER_REV 200
+
+bool statusSensorNow = false;
+bool statusSensorPrev = false;
+unsigned long dispenseTime, dispenseTimePrev, sphereCountTime, sphereCountTimePrev;
+const long dispenseDuration = 500;
+const long sphereCountDuration = 500;
+
+#define I2C_ADDRESS 0x3C
+#define RST_PIN -1
+SSD1306AsciiWire oled;
+
+Stepper stepMot(STEPS_PER_REV, stepPin1, stepPin2, stepPin3, stepPin4);
+
+enum State {
+  RUNNING,
+  STOP
+};
+
+// ethernet interface mac address, must be unique on the LAN
+static byte mymac[] = { 0x74,0x39,0x70,0x2D,0x32,0x31 };
+static byte myip[] = { 192,168,0,5};
+State state = STOP;
+int ballNumber = 8;
+int restBallNumber = 0;
+bool detected = false;
+
+static byte Ethernet::buffer[200];                      
+BufferFiller bfill;
+
+static int getIntArg(const char* data, const char* key, int value =-1) {
+    char temp[10];
+    if (ether.findKeyVal(data + 7, temp, sizeof temp, key) > 0)
+        value = atoi(temp);
+    return value;
+}
+
+inline void start_work() {
+  state = RUNNING;
+  restBallNumber = ballNumber;
+  writeDisplay(1);
+}
+
+inline void stop_work() {
+  restBallNumber = 0;
+  state = STOP;
+  writeDisplay(1);
+}
+
+inline void change_number(int n) {
+  if (n > 0 && n <= 100) {
+    ballNumber = n;
+    writeDisplay(1);
+  }
+}
+
+static void start_work(const char* data, BufferFiller& buf) {
+  DynamicJsonDocument response(256);
+  if (state != STOP) {
+    response["status"] = "error";
+    response["reason"] = F("The machine is running.");
+    buf.emit_p(PSTR(
+            "HTTP/1.0 400 Bad Request\r\n"
+            "Content-Type: application/json\r\n"
+            "\r\n"));
+  } else {
+    start_work();
+    response["status"] = "success";
+    writeDisplay(1);
+    buf.emit_p(PSTR(
+        "HTTP/1.0 200 OK\r\n"
+        "Content-Type: application/json\r\n"
+        "\r\n"));
+  }
+  serializeJson(response, buf);
+}
+
+static void stop_work(const char* data, BufferFiller& buf) {
+  DynamicJsonDocument response(256);
+  if (state != RUNNING) {
+    response["status"] = "error";
+    response["reason"] = "The machine is not running.";
+    buf.emit_p(PSTR(
+            "HTTP/1.0 400 Bad Request\r\n"
+            "Content-Type: application/json\r\n"
+            "\r\n"));
+  } else {
+    response["status"] = "success";
+    response["rest_ball_number"] = restBallNumber;
+    stop_work();
+    writeDisplay(1);
+    buf.emit_p(PSTR(
+        "HTTP/1.0 200 OK\r\n"
+        "Content-Type: application/json\r\n"
+        "\r\n"));
+  }
+  serializeJson(response, buf);
+}
+
+static void change_number(const char* data, BufferFiller& buf) {
+  DynamicJsonDocument response(256);
+  int n = getIntArg(data, "n");
+  if (state != STOP) {
+    response["status"] = "error";
+    response["reason"] = "The machine is still running.";
+    buf.emit_p(PSTR(
+            "HTTP/1.0 400 Bad Request\r\n"
+            "Content-Type: application/json\r\n"
+            "\r\n"));
+  } else if (n == -1) {
+    response["status"] = "error";
+    response["reason"] = "Missing ball number.";
+    buf.emit_p(PSTR(
+        "HTTP/1.0 400 Bad Request\r\n"
+        "Content-Type: application/json\r\n"
+        "\r\n"));
+  } else if (n < 0 || n > 100) {
+    response["status"] = "error";
+    response["reason"] = "Unexpected ball number. Expected ball number range: 1~100.";
+    buf.emit_p(PSTR(
+        "HTTP/1.0 400 Bad Request\r\n"
+        "Content-Type: application/json\r\n"
+        "\r\n"));
+  } else {
+    delay(1);
+    change_number(n);
+    response["status"] = "success";
+    response["number"] = n;
+    buf.emit_p(PSTR(
+        "HTTP/1.0 200 OK\r\n"
+        "Content-Type: application/json\r\n"
+        "\r\n"));
+  }
+  serializeJson(response, buf);
+}
+
+static void get_number(const char* data, BufferFiller& buf) {
+  DynamicJsonDocument response(256);
+  response["status"] = "success";
+  response["number"] = ballNumber;
+  buf.emit_p(PSTR(
+      "HTTP/1.0 200 OK\r\n"
+      "Content-Type: application/json\r\n"
+      "\r\n"));
+  serializeJson(response, buf);
+}
+
+static void page_404(const char* data, BufferFiller& buf) {
+  DynamicJsonDocument response(256);
+  response["status"] = "error";
+  response["reason"] = "Requested endpoint not found.";
+  bfill.emit_p(PSTR(
+      "HTTP/1.0 404 Not Found\r\n"
+      "Content-Type: application/json\r\n"
+      "\r\n"));
+   serializeJson(response, buf);
+}
+
+
+void writeDisplay(int mode) {
+  switch (mode) {
+    case 1: //default screen update
+      oled.clear();
+      oled.set1X();
+      if (state == STOP) {
+        // first row
+        oled.println(F("A-LAB SPHERE DISPENSER"));
+  
+        // second row
+        oled.print("Amount:  ");
+        oled.set2X();
+        oled.println(ballNumber);
+        // third row
+        oled.set1X();
+        oled.print(F("Press GO to dispense"));
+      } else {
+        // first row
+        oled.println(F(">> Dispenser RUNNING"));
+  
+        // second row
+        oled.print(F("Done/Tot  "));
+        oled.set2X();
+        oled.print(ballNumber - restBallNumber);
+        oled.print("/");
+        oled.println(ballNumber);
+        oled.set1X();
+        oled.print(F("Press GO to stop"));
+      }
+      break;
+      
+    default: //initial screen
+      oled.clear();
+      // first row
+      oled.println(F("     >> A-LAB <<"));
+      oled.println(F(""));
+      oled.println(F("       SPHERE"));
+      oled.println(F("      DISPENSER"));
+      delay(2000);
+      break;
+  }
+}
+
+void dispense(Stepper myStepper,int stepperSpeed, int percent=25){
+  if (stepperSpeed > 0) {
+    myStepper.setSpeed(stepperSpeed);
+    // step n/100 of a revolution:
+    myStepper.step(-percent*STEPS_PER_REV/100);
+  }
+  digitalWrite(stepPin1, LOW);
+  digitalWrite(stepPin2, LOW);
+  digitalWrite(stepPin3, LOW);
+  digitalWrite(stepPin4, LOW);
+}
+
+void addSphereCount(){
+  sphereCountTime=millis();
+  if ((sphereCountTime-sphereCountTimePrev)>sphereCountDuration){
+    sphereCountTimePrev=sphereCountTime;
+    restBallNumber--;
+    Serial.print("+1 ");
+    Serial.println("Dispensed one.");
+    Serial.println(F(""));
+    Serial.print(restBallNumber);
+    Serial.println(F(" remaining."));
+  }
+  else{
+  }
+}
+
+COROUTINE(dispenseBalls) {
+  COROUTINE_LOOP() {
+    COROUTINE_DELAY(30);
+    if (state == RUNNING && restBallNumber > 0) {
+      dispenseTime=millis();
+      if((dispenseTime-dispenseTimePrev) > dispenseDuration){
+        dispenseTimePrev=dispenseTime;
+        dispense(stepMot, 30, 5);
+      }
+
+      if (restBallNumber > 0){
+        writeDisplay(1);
+      }
+      
+    } else {
+      COROUTINE_DELAY(100);
+    }
+    if (restBallNumber <= 0 and state == RUNNING) {
+      state = STOP;
+      Serial.print(F("Dispensed "));
+      Serial.print(ballNumber-restBallNumber);
+      Serial.println(F(" spheres."));
+      writeDisplay(1);
+    }
+  }
+}
+
+COROUTINE(handleSerialRequest) {
+  COROUTINE_LOOP() {
+    COROUTINE_DELAY(30);
+    String text;
+
+    if (Serial.available() != 0) {
+      //Serial.println(Serial.read());
+      //reqSpheres = Serial.parseInt(); //Read the data the user has input
+      text = Serial.readString();
+      int reqSpheres = text.toInt();
+      Serial.print(F("Received serial command. Dispensing "));
+      Serial.print(reqSpheres);
+      Serial.println(F(" spheres."));
+      change_number(reqSpheres);
+      start_work();
+    }
+  }
+}
+
+COROUTINE(handleButtonChange) {
+  COROUTINE_LOOP() {
+    COROUTINE_DELAY(30);
+      if (digitalRead(button1) == HIGH) {
+        COROUTINE_AWAIT(digitalRead(button1) != HIGH);
+        if (ballNumber < 100) {
+          change_number(ballNumber + 1);
+          Serial.print(F("Added one. Now you have "));
+          Serial.print(ballNumber);
+          Serial.println(F(" spheres to go."));
+        }
+    }
+  
+    if (digitalRead(button2) == HIGH) {
+      COROUTINE_AWAIT(digitalRead(button2) != HIGH);
+      if (ballNumber > 0) {
+        change_number(ballNumber - 1);
+        Serial.print(F("Removed one. Now you have "));
+        Serial.print(ballNumber);
+        Serial.println(F(" spheres to go."));
+      }
+    }
+  
+    if (digitalRead(button3) == HIGH) {
+      COROUTINE_AWAIT(digitalRead(button3) == HIGH);
+      if (state == STOP) {
+        Serial.print(F("GO button pressed. Dispensing "));
+        Serial.print(ballNumber);
+        Serial.println(F(" now."));
+        start_work();
+      } else {
+        Serial.print(F("Receive stop signal. We need "));
+        Serial.print(restBallNumber);
+        Serial.println(F(" more balls."));
+        stop_work();
+      }
+    }
+  }
+}
+
+COROUTINE(handleRemoteRequest) {
+  COROUTINE_LOOP() {
+    COROUTINE_DELAY(30);
+    word len = ether.packetReceive();
+    word pos = ether.packetLoop(len);
+  
+    if (pos) {
+      bfill = ether.tcpOffset();
+      char* data = (char *) Ethernet::buffer + pos;
+  //    Serial.println(data);
+      // receive buf hasn't been clobbered by reply yet
+      if (strncmp("GET /start", data, 10) == 0) {
+        start_work(data, bfill);
+      }
+      else if (strncmp("GET /stop", data, 9) == 0) {
+        stop_work(data, bfill);
+      }
+      else if (strncmp("GET /change", data, 11) == 0) {
+        change_number(data, bfill);
+      }
+      else if (strncmp("GET /num", data, 8) == 0) {
+        get_number(data, bfill);  
+      } else {
+        page_404(data, bfill);
+      }
+      ether.httpServerReply(bfill.position()); // send web page data
+    }  
+  }  
+}
+
+void setup () {
+  delay(1000);
+  Wire.begin();
+  oled.begin(&Adafruit128x32, I2C_ADDRESS);
+  oled.setFont(Adafruit5x7);
+  writeDisplay(0);
+  Serial.begin(9600);
+  if (ether.begin(sizeof Ethernet::buffer, mymac, SS) == 0){
+    Serial.println(F("Failed to access Ethernet controller"));
+  }
+  ether.staticSetup(myip);
+
+//  attachInterrupt(digitalPinToInterrupt(IRPin1), addSphereCount, RISING);
+//  attachInterrupt(digitalPinToInterrupt(IRPin2), addSphereCount, RISING);
+//  pinMode (button1, INPUT);
+//  pinMode (button2, INPUT);
+//  pinMode (button3, INPUT);
+  pinMode(stepPin1, OUTPUT);
+  pinMode(stepPin2, OUTPUT);
+  pinMode(stepPin3, OUTPUT);
+  pinMode(stepPin4, OUTPUT);
+  
+  digitalWrite(stepPin1, LOW);
+  digitalWrite(stepPin2, LOW);
+  digitalWrite(stepPin3, LOW);
+  digitalWrite(stepPin4, LOW);
+
+  Serial.println(F("Type the number of spheres you want"));
+  writeDisplay(1);
+  
+  dispenseTime = millis();
+  dispenseTimePrev = millis();
+
+  sphereCountTime = millis();
+  sphereCountTimePrev = millis();
+}
+
+void loop () {
+  dispenseBalls.runCoroutine();
+//  handleButtonChange.runCoroutine();
+  handleSerialRequest.runCoroutine();
+  handleRemoteRequest.runCoroutine();
+}
---
+++ arduino_programs/door_controller/soft_R07_CD/soft_R07_CD.ino
@@ -0,0 +1,380 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include<ArduinoJson.h>
+#include <arduino-timer.h>
+#include <AceRoutine.h>
+
+Timer<3> timer;
+
+String clientMsg = "";
+//String reason = "";
+String command = "";
+String reply="";
+int lsC=0;
+int lsD=0;
+
+int serialwait = 0;
+int serialwaitingtime = 6;
+byte mac[] = { 0x00, 0x52, 0x16, 0x64, 0xC0, 0x11 };
+//00:52:16:64:C0:39
+
+IPAddress serverIP(192, 168, 0, 42);
+int serverPort = 8888;
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+EthernetServer server(serverPort);
+EthernetClient client;
+
+// Furnace closing time
+#define serialwaitingtime 5 //time in seconds to wait for the serial connection to be stablished, or it will be canceled.
+
+// Motor C ; Furnace C
+
+#define enC 2 //enable motor C
+#define in2 3
+#define in1 A0
+#define dlsC 5 //door limit switch for box furnace C
+
+// Motor D ; Furnace D
+
+#define enD 6 //enable motor D
+#define in4 7
+#define in3 8
+#define dlsD 9 //door limit switch for box furnace D
+
+//pins 4, 10, 11, 12, 13 are reserved for the ethernet shield
+
+unsigned long currentTime,previousTime,duration;
+const long maxOpeningCDuration = 21000; // 
+const long maxOpeningDDuration = 21000; // 
+const long closingDurationC = 25000; //
+const long closingDurationD = 25000; //
+
+String FurnaceCState="Closed", FurnaceDState="Closed";
+String prevFurnaceCState="Closed", prevFurnaceDState="Closed";
+
+void endFurnaceRoutine() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, HIGH);
+  digitalWrite(enC, LOW);
+  digitalWrite(in4, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(enD, LOW);
+}
+
+auto runningTimer=timer.in(1000,endFurnaceRoutine);
+
+enum State {
+  RUNNING,
+  STOP,
+  ERR
+};
+
+State state = STOP;
+
+String openFurnaceC(){
+  Serial.print("Opening furnace C...");
+  reply="Opening furnace C...";
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  state = RUNNING;
+  digitalWrite(enC, HIGH);
+  runningTimer = timer.in(maxOpeningCDuration,emergencyStopOpeningFurnaceC);
+  return reply;
+}
+
+void gracefulStopOpeningFurnaceC(){
+  timer.cancel(runningTimer);
+  state = STOP;
+  FurnaceCState = "Open";
+  endFurnaceRoutine();
+  Serial.println("Furnace C opened gracefully.");
+}
+
+void emergencyStopOpeningFurnaceC(){
+  state = ERR;
+  timer.cancel(runningTimer);
+  FurnaceCState = "Open";
+//  reason = "BOX FURNACE C STOPPED DUE TO TIME PROTECTION!"; Memory Overrun, Cannot use!!
+  endFurnaceRoutine();
+  //Auto closing furnace for safety
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(enC, HIGH);
+  runningTimer = timer.in(closingDurationC,EmergencyCloseFurnaceC);
+}
+
+void EmergencyCloseFurnaceC(){
+  FurnaceCState="Unknown";
+  timer.cancel(runningTimer);
+  state = ERR;
+  endFurnaceRoutine();
+  Serial.println("Furnace C emergency closed.");
+}
+
+String closeFurnaceC(){
+  Serial.print("Closing furnace C...");
+  reply="Closing furnace C...";
+  state = RUNNING;
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(enC, HIGH);
+  runningTimer = timer.in(closingDurationC,gracefulStopClosingFurnaceC);
+  return reply;
+}
+
+void gracefulStopClosingFurnaceC(){
+  FurnaceCState="Closed";
+  timer.cancel(runningTimer);
+  state = STOP;
+  endFurnaceRoutine();
+  Serial.println("Furnace C closed gracefully.");
+}
+
+String openFurnaceD(){
+  Serial.print("Opening furnace D...");
+  reply="Opening furnace D...";
+  digitalWrite(in4, HIGH);
+  digitalWrite(in3, LOW);
+  state = RUNNING;
+  digitalWrite(enD, HIGH);
+  runningTimer = timer.in(maxOpeningDDuration,emergencyStopOpeningFurnaceD);
+  return reply;
+}
+
+void gracefulStopOpeningFurnaceD(){
+  timer.cancel(runningTimer);
+  state = STOP;
+  FurnaceDState = "Open";
+  endFurnaceRoutine();
+  Serial.println("Furnace D opened gracefully.");
+}
+
+void emergencyStopOpeningFurnaceD(){
+  state = ERR;
+  timer.cancel(runningTimer);
+  FurnaceDState = "Open";
+//  reason = "BOX FURNACE D STOPPED DUE TO TIME PROTECTION!"; Cannot use, Memory Overrun!
+  endFurnaceRoutine();
+  //Auto closing furnace for safety
+  digitalWrite(in4, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(enD, HIGH);
+  runningTimer = timer.in(closingDurationD,EmergencyCloseFurnaceD);
+}
+
+void EmergencyCloseFurnaceD(){
+  FurnaceDState="Unknown";
+  timer.cancel(runningTimer);
+  state = ERR;
+  endFurnaceRoutine();
+  Serial.println("Furnace D emergency closed.");
+}
+
+String closeFurnaceD(){
+  Serial.print("Closing furnace D...");
+  reply="Closing furnace D...";
+  state = RUNNING;
+  digitalWrite(in4, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(enD, HIGH);
+  runningTimer = timer.in(closingDurationD,gracefulStopClosingFurnaceD);
+  return reply;
+}
+
+void gracefulStopClosingFurnaceD(){
+  FurnaceDState = "Closed";
+  timer.cancel(runningTimer);
+  state = STOP;
+  endFurnaceRoutine();
+  Serial.println("Furnace D closed gracefully.");
+}
+
+String checkStatus(){
+  Serial.println(" I received the command Status");
+  reply="State: ";
+  if (state == ERR) {
+//    Serial.println(reason);
+    reply=reply+"ERROR; ";
+  }
+  else if(state == RUNNING){
+    reply=reply+"RUNNING; ";
+  }
+  else if(state == STOP){
+    reply=reply+"STOP; ";
+  }
+  reply=reply+"Furnace C: "+FurnaceCState+"; "+"Furnace D: "+FurnaceDState+";";
+  Serial.println(reply);
+  return reply;
+}
+
+COROUTINE(checkLS_coroutine) {
+  COROUTINE_LOOP() {
+    COROUTINE_DELAY(30);
+    if(digitalRead(dlsC) == 1){
+    Serial.println("Furnace C is HIGH");
+    if(command=="Open C" and state!=ERR){
+      gracefulStopOpeningFurnaceC();
+      }
+    }
+    if(digitalRead(dlsD) == 1){
+      Serial.println("Furnace D is HIGH");
+      if(command=="Open D" and state!=ERR){
+        gracefulStopOpeningFurnaceD();
+      }
+    }
+  }
+}
+
+COROUTINE(main_coroutine) {
+  COROUTINE_LOOP() {
+    COROUTINE_DELAY(30);
+    // listen for incoming clients
+    EthernetClient client = server.available();
+    if (client) {
+      Serial.println("Client connected.");
+      while (client.connected()) {
+        if (client.available()) {
+          char c = client.read();
+          clientMsg += c; //store the received chracters in a string
+          //if the character is an "end of line" the whole message is recieved
+          if (c == '\n') {
+            clientMsg.trim();
+            Serial.println("RECEIVED>>" + clientMsg); //print it to the serial
+            if (clientMsg == "Open C" and state!=ERR and state!=RUNNING) {
+              if (FurnaceCState == "Closed"){
+                reply=openFurnaceC();
+                command="Open C";
+                client.print(reply);
+              }
+              else if (FurnaceCState == "Open"){
+                reply=checkStatus();
+                client.print(reply);
+              }
+            }
+            else if (clientMsg == "Close C" and state!=ERR and state!=RUNNING) {
+              if (FurnaceCState == "Open"){
+                reply=closeFurnaceC();
+                command="Close C";
+                client.print(reply);
+              }
+              else if (FurnaceCState == "Closed"){
+                reply=checkStatus();
+                client.print(reply);
+              }
+            }
+            else if (clientMsg == "Open D" and state!=ERR and state!=RUNNING) {
+              if (FurnaceDState == "Closed"){
+                reply=openFurnaceD();
+                command="Open D";
+                client.print(reply);
+              }
+              else if (FurnaceDState == "Open"){
+                reply=checkStatus();
+                client.print(reply);
+              }
+            }
+            else if (clientMsg == "Close D" and state!=ERR and state!=RUNNING) {
+              if (FurnaceDState == "Open"){
+                reply=closeFurnaceD();
+                command="Close D";
+                client.print(reply);
+              }
+              else if (FurnaceDState == "Closed"){
+                reply=checkStatus();
+                client.print(reply);
+              }
+            }
+            else if ((clientMsg == "Open C" or clientMsg == "Open D" or clientMsg == "Close C" or clientMsg == "Close D") and (state==ERR or state==RUNNING)){
+              reply=checkStatus();
+              client.print(reply);
+            }
+            else if (clientMsg == "Status") {
+              reply=checkStatus();
+              client.print(reply);
+            }
+            else{
+              Serial.println("Wrong command");
+              client.print("Wrong command");
+            }
+            clientMsg = "";
+            reply="";
+          }
+        }
+      }
+      // give the Client time to receive the data
+      delay(10);
+      // close the connection:
+      client.stop();
+      Serial.println(("Client disconnected."));
+    }
+  }
+}
+
+void setup()
+{
+  timer.cancel(runningTimer);
+  Serial.end(); //restarting serial communication with every setup
+  Serial.begin(9600);
+  pinMode(enC, OUTPUT);
+  pinMode(enD, OUTPUT);
+  pinMode(dlsC, INPUT_PULLUP);
+  pinMode(dlsD, INPUT_PULLUP);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(in3, OUTPUT);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, HIGH);
+  digitalWrite(enC, LOW);
+  digitalWrite(in4, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(enD, LOW);
+  //Initialize the LED as an output
+  //digitalWrite(LED_BUILTIN, LOW);
+  // turn the LED off by making the voltage LOW
+  // start the serial for debugging
+  while (!Serial) {
+    delay(1000);
+    if (serialwait == serialwaitingtime) {
+      break;
+    }
+    serialwait++;
+  }
+  serialwait = 0;
+  Serial.println(("Serial started. Now starting ethernet"));
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, serverIP);
+  server.begin();
+  Serial.print(("Ethernet/server started started at "));
+  Serial.print(Ethernet.localIP());
+  Serial.print((" : "));
+  Serial.print(serverPort);
+  Serial.print((" and gateway "));
+  Serial.print(Ethernet.gatewayIP());
+  Serial.println((" ."));
+  // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println(("Ethernet shield was not found. The code does nothing from now on."));
+    while (true) {
+      delay(1000); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    while ((Ethernet.linkStatus() == LinkOFF)) {
+      Serial.println(("Ethernet cable is not connected. Trying again in 2 seconds."));
+      delay(2000);
+    }
+    Serial.println(("Ethernet cable is now detected. Resuming the routine now..."));
+  }
+  Serial.println(("Please write either Open C, Close C, Open D, Close D, or Status."));
+  Serial.println(("Write debug or deployment to set mode"));
+}
+
+void loop()
+{
+  timer.tick();
+  main_coroutine.runCoroutine();
+  checkLS_coroutine.runCoroutine();
+}
