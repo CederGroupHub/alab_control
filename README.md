b834f3d6c51acafdf8154cd51716975ceda6e20c
Rishi Kumar <re.kumar@icloud.com>
2023-08-31 10:13:19 -1000

    fix setup requirements

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ This python package includes drivers needed to control devices in Ceder group's 
 python setup.py install
 ```
 
+Note that at least one requirement is only available in the Ceder group's private repository. These requirements are contained in the `requirements_ceder.txt` file and can be included by installing with `pip install alab_control[ceder]`. Alternatively, you can install the requirements manually with
+
+```shell
+pip install -r requirements_ceder.txt
+```
+
+
+
 ## Test
 ```shell
 python -m unittest
--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
+from pathlib import Path
 from setuptools import setup, find_packages
+from typing import List
+
+THIS_DIR = Path(__file__).parent
+
+with open(THIS_DIR / "README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+def read_requirements(filepath: Path) -> List[str]:
+    with open(filepath, encoding="utf-8") as fd:
+        return [
+            package.strip("\n")
+            for package in fd.readlines()
+            if not package.startswith("#")
+        ]
+
+requirements = read_requirements(THIS_DIR / "requirements.txt")
+requirements_ceder = read_requirements(THIS_DIR / "requirements_ceder.txt") #these are internal Ceder group packages that can only be installed with access to private git repos. 
 
 setup(
     name='alab_control',
@@ -8,8 +27,7 @@ setup(
     python_requires=">3.6",
     description='Drivers for alab devices',
     zip_safe=False,
-    install_requires=[
-        "pyModbusTCP",
-    ],
+    install_requires=requirements,
+    extras_require={"ceder": requirements_ceder},
     include_package_data=True
 )
