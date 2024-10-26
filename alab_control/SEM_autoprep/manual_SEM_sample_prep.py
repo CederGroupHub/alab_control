9284fc49e87f080411846e47a318f2870374af02
D Milsted <eng.dmilsted@gmail.com>
2024-10-25 17:18:30 -0700

    code update
    just updating codes that were changed so I can keep working on new features

--- alab_control/SEM_autoprep/manual_SEM_sample_prep.py
+++ alab_control/SEM_autoprep/manual_SEM_sample_prep.py
@@ -5,15 +5,16 @@ import time
 import sys
 
 # USE THE VARIABLES BELOW TO SETUP YOUR EXPERIMENT #
-EXPOSURE_DISTANCE = 0 #value between -25 and +25, measured in mm, from the top of the crucible
+EXPOSURE_DISTANCE = 21 #value between -25 and +25, measured in mm, from the top of the crucible
 EXPOSURE_VOLTAGE = "10000" #value between 01000 and 30000, measured in volts
 EXPOSURE_TIME = "05000" #value between 250 and 99999, measured in millli seconds
-DESTINATION = 1 #0 is to return to stub holder. 1 is to place on phenom stage
+DESTINATION = 0 #0 is to return to stub holder. 1 is to place on phenom stage
 MANUAL_STUB_LOAD = 0 #0 is to automatically pick the stubs from the stub holder. 1 is to manually place them on the needle
+TAKE_STUB_PIC = 0 #0 is to skip taking a picture of the exposed stub. 1 takes the pic
 
 
 #constants for safe operation
-crucible_exp_dist = 89
+crucible_exp_dist = 71 #71 is the standard
 MAX_EXPOSURE_DISTANCE = 25.0
 CRUCIBLE_HEIGHT = 39
 
@@ -46,7 +47,7 @@ class SamplePrepEnder3(Ender3):
     PREP_EXP = (60, 48, None)
     STANDBY = (15, 15, 15)
     Z_STANDBY = (None, None, 5)
-    STUB = (50.5, 125.5, None)
+    STUB = (51.3, 126.8, None)
     Z_STUB_PICK2 = (None, None, 61)
     Z_STUB_PICK1 = (None, None, 67)
     Z_STUB_PICK0 = (None, None, 70)
@@ -54,9 +55,9 @@ class SamplePrepEnder3(Ender3):
     Z_LASER = (None, None, 62)
     MIRROR = (103, 97, None)
     Z_MIRROR = (None, None, 65)
-    ROTATOR_0 = (120, 109.5, None)
-    Z_ROTATOR_0 = (None, None, 66)
-    ROTATOR_1 = (134, 109.5, 66) #Z value MUST agree with Z_ROTATOR_0
+    ROTATOR_0 = (120, 111.5, None)
+    Z_ROTATOR_0 = (None, None, 66.5)
+    ROTATOR_1 = (134, 111.5, 66.5) #Z value MUST agree with Z_ROTATOR_0
     GRIPPER_0 = (110, 131, None)
     Z_GRIPPER_0 = (None, None, 51)
     GRIPPER_1 = (95, None, None)
@@ -90,7 +91,8 @@ if __name__ == "__main__":
     else:
         time.sleep(PAUSE)
         print("Done.")
-    
+
+    time.sleep(2)    
 
     try:
         r = SamplePrepEnder3("COM7") 
@@ -111,8 +113,8 @@ if __name__ == "__main__":
     r.speed = SPEED_NORMAL
     print("Done.")
     r.moveto(*r.STUB)
-    print("Turning on vacuum pump 1...")
-    reply = send_command("SEMPREPVAC1")
+    print("Turning on vacuum pump 2 (TEMPORARY SOLUTION: PUMP 1 IS STANDARD - SEMPREPVAC1)...")
+    reply = send_command("TEMPREPVAC1")
     print("Control panel replied >>" + reply)
     time.sleep(PAUSE)
     r.moveto(*r.Z_STUB_PICK2)
@@ -125,37 +127,7 @@ if __name__ == "__main__":
     r.speed = SPEED_NORMAL
     r.moveto(*r.Z_STANDBY)
 
-    '''
-    while True:
-        picked_confirm = input(
-            "Stub picked? C to continue, R to try again, M for manual, A to abort: "
-        )
-        if picked_confirm.lower() == "a":
-            send_command("STANDBY")
-            print("Aborted.")
-            sys.exit()
-        elif picked_confirm.lower() == "c":
-            break
-        elif picked_confirm.lower() == "r":
-            r.speed = SPEED_LOW
-            r.moveto(*r.Z_STUB_PICK1)
-            r.moveto(*r.Z_STUB_PICK0)
-            picked_confirm = input(
-                "Stub picked? If not, R to try again, C to continue: "
-            )
-            if picked_confirm.lower() == "c":
-                break
-        elif picked_confirm.lower() == "m":
-            r.speed = SPEED_NORMAL
-            r.moveto(*r.Z_STANDBY)
-            #r.moveto(*r.HOME)
-            picked_confirm = input(
-                "Press enter when the stub is properly attached."
-            )
-            break
-        else:
-            print("Invalid choice. Please try again.")
-    '''
+
     stub_pick_trials = 0
     while True:
         if stub_pick_trials >= 2:
@@ -204,15 +176,14 @@ if __name__ == "__main__":
     r.moveto(*r.Z_STANDBY)
     #r.moveto(*r.STANDBY)
 
-    
-    print("\nTaking a picture of the stub...")
-    r.moveto(*r.MIRROR)
-    r.moveto(*r.Z_MIRROR)
-    print("\n... **CLICK** ...")
-    time.sleep(2)
-    print("\nDone.")
-    r.moveto(*r.Z_STANDBY)
-    #r.moveto(*r.STANDBY)
+    if TAKE_STUB_PIC == 1:
+        print("\nTaking a picture of the stub...")
+        r.moveto(*r.MIRROR)
+        r.moveto(*r.Z_MIRROR)
+        print("\n... **CLICK** ...")
+        time.sleep(2)
+        print("\nDone.")
+        r.moveto(*r.Z_STANDBY)
     
 
     if DESTINATION == 0:
@@ -260,6 +231,7 @@ if __name__ == "__main__":
         send_command("PHLIDMVL150")
         r.moveto(*r.STANDBY)
 
+        
 
 
 
---
+++ alab_control/SEM_autoprep/manual_TEM_sample_prep_TARA.py
@@ -0,0 +1,187 @@
+from alab_control.ender3 import Ender3
+import serial
+import socket
+import time
+import sys
+
+# USE THE VARIABLES BELOW TO SETUP YOUR EXPERIMENT #
+EXPOSURE_DISTANCE = -8 #value between -25 and +25, measured in mm, from the top of the crucible
+EXPOSURE_VOLTAGE = "10000" #value between 01000 and 30000, measured in volts
+EXPOSURE_TIME = "15000" #value between 250 and 99999, measured in millli seconds
+
+
+#constants for safe operation
+crucible_exp_dist = 75 #71 is the standard
+MAX_EXPOSURE_DISTANCE = 10.0
+CRUCIBLE_HEIGHT = 39
+
+#ADD CODE TO EVALUATE DISTANCE BEFORE THE SYSTEM STARTS WITH THE MACHINE
+
+#speed values
+SPEED_VLOW = 0.005
+SPEED_LOW = 0.02
+SPEED_NORMAL = 0.5
+
+#sleep times
+PAUSE = 4
+PAUSE_VAC = 16
+
+def send_command(message, host = '192.168.0.46', port = '8888'):
+    port=int(port)
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host,port))
+        s.sendall(message.encode())
+        data = s.recv(1024)
+        decoded = data.decode('utf-8')
+        print('Socket reply>>'+decoded)
+
+class SamplePrepEnder3(Ender3):
+    """This class is for controlling the Ender3 3D printer for sample preparation."""
+
+
+    # positions
+    HOME = (150, 5, 5)
+    STUB = (140, 5, None)
+    PREP_EXP = (8, 42, None)
+    Z_PREP_EXP = (None, None, 30)
+    STANDBY = (15, 15, 15)
+    Z_STANDBY = (None, None, 15)
+    Z_STUB_PICK0 = (None, None, 5)
+    Z_STUB_PICK1 = (None, None, 5)
+    Z_STUB_PICK2 = (None, None, 5)
+
+
+
+
+
+
+    #we need 18 clean stubs
+    #we need 18 prepared stubs
+    #we need 40 grid positions
+
+
+
+
+if __name__ == "__main__":
+    print('\033c') #cleaning the terminal screen
+    print(
+        "**************************************************************************** \n "
+    )
+    print("Hello! This code will guide you on using the Automated Sample Prep \n")
+    print(
+        "**************************************************************************** \n "
+    )
+
+    print("Starting the control panel...")
+    try:
+        send_command("STANDBY")
+    except Exception as var_error:
+        print(f"The execution was halted, an error occurred: {var_error}")
+        sys.exit()
+    else:
+        time.sleep(PAUSE)
+        print("Done.")
+    
+
+    try:
+        r = SamplePrepEnder3("COM7") 
+    except Exception as var_error:
+        print(f"An error occurred: {var_error}")
+        print(f"These are the available connections: \n")
+        ports = list(serial.tools.list_ports.comports())
+        for p in ports:
+            print(p)
+        input("\n Press enter to end the program.")
+        exit()
+
+    print("Printer is resetting the positioning system. Please wait... \n")
+    r.gohome()
+
+    print("Homing head unit. Please wait...")
+    r.moveto(*r.HOME)
+    r.speed = SPEED_NORMAL
+    print("Done.")
+    r.moveto(*r.STUB)
+    
+    #time.sleep(PAUSE)
+    r.moveto(*r.Z_STUB_PICK2)
+    r.speed = SPEED_LOW
+    r.moveto(*r.Z_STUB_PICK1)
+    r.speed = SPEED_VLOW
+    r.moveto(*r.Z_STUB_PICK0)
+    send_command("TEMPREPVAC1")
+    time.sleep(PAUSE)
+    r.moveto(*r.Z_STUB_PICK1)
+
+    r.speed = SPEED_NORMAL
+    r.moveto(*r.Z_STANDBY)
+
+    while True:
+        picked_confirm = input(
+            "ACTION NEEDED: Manually place the grid on the needle and: C to continue, A to abort: "
+        )
+        if picked_confirm.lower() == "a":
+            send_command("STANDBY")
+            print("Aborted.")
+            sys.exit()
+        elif picked_confirm.lower() == "c":
+            break
+        
+        elif picked_confirm.lower() == "r":
+            send_command("STANDBY")
+            r.speed = SPEED_LOW
+            r.moveto(*r.Z_STUB_PICK1)
+            r.moveto(*r.Z_STUB_PICK0)
+            send_command("TEMPREPVAC1")
+            time.sleep(PAUSE)
+            picked_confirm = input(
+                "ACTION NEEDED: Grid picked? If not, R to try again, C to continue: "
+            )
+            if picked_confirm.lower() == "c":
+                break
+        elif picked_confirm.lower() == "m":
+            r.speed = SPEED_NORMAL
+            r.moveto(*r.Z_STANDBY)
+            r.moveto(*r.HOME)
+            picked_confirm = input(
+                "Press enter when the grid is properly attached."
+            )
+            break
+        
+        else:
+            print("Invalid choice. Please try again.")
+
+    r.speed = SPEED_NORMAL
+    r.moveto(*r.Z_STANDBY)
+
+    r.moveto(*r.HOME)
+    r.moveto(*r.PREP_EXP)
+    #r.moveto(*r.Z_PREP_EXP)
+    r.moveto(z=crucible_exp_dist - CRUCIBLE_HEIGHT)
+
+    r.moveto(z=crucible_exp_dist -  CRUCIBLE_HEIGHT + EXPOSURE_DISTANCE)
+    r.speed = SPEED_LOW
+    r.moverel(z=EXPOSURE_DISTANCE)
+    print("\n***** STUB WILL BE EXPOSED FOR " + EXPOSURE_TIME + " MILLI SECONDS *****")
+    time.sleep(PAUSE)
+    send_command("EXPOSURV"+EXPOSURE_VOLTAGE+"T"+EXPOSURE_TIME)
+    print("\n***** EXPOSING *****")
+    time.sleep(int(EXPOSURE_TIME)/1000+2)
+    print("\n***** EXPOSING PROCEDURE FINISHED *****")
+    r.speed = SPEED_NORMAL
+    r.moveto(*r.Z_STANDBY)
+    r.moveto(*r.HOME)
+    r.moveto(*r.STUB)
+
+    r.moveto(*r.Z_STUB_PICK1)
+    r.speed = SPEED_VLOW
+    r.moveto(*r.Z_STUB_PICK0)
+    picked_confirm = input(
+                "ACTION: Manually pick the grid and press enter"
+            )
+    send_command("STANDBY")
+    #time.sleep(PAUSE_VAC)
+    r.speed = SPEED_NORMAL
+    r.moveto(*r.Z_STANDBY)
+    r.moveto(*r.STANDBY)
