e6cc6a69cf27f8d3673e179eecc54fd0fbd1b982
Yuxing Fei <yuxingfei@berkeley.edu>
2024-11-08 17:26:37 -0800

    add dh codes

--- alab_control/dh_linear_rail/dh_linear_rail.py
+++ alab_control/dh_linear_rail/dh_linear_rail.py
@@ -6,31 +6,18 @@ from pymodbus.exceptions import ModbusException
 from enum import Enum
 
 
-class InitializationStatus(Enum):
+class RailInitializationStatus(Enum):
     NOT_INITIALIZED = 0
     INITIALIZED = 1
     INITIALIZING = 2
 
 
-class RotationDirection(Enum):
-    CLOCKWISE = 1
-    COUNTERCLOCKWISE = -1
-
-
-class RotationStatus(Enum):
-    MOVING = 0
-    REACHED = 1
-    BLOCKED = 3
-
-
-class GripperStatus(Enum):
+class RailStatus(Enum):
     MOVING = 0
     ARRIVED = 1
-    GRASPED = 2
-    DROPPED = 3
 
 
-class GripperController:
+class LinearRailController:
     def __init__(self, port, slave_address=1, baudrate=115200):
         # Setup Modbus RTU client
         self.client = ModbusClient(
@@ -46,196 +33,89 @@ class GripperController:
         if not self.client.connect():
             raise ModbusException("Unable to connect to the gripper")
 
-        self.load_defaults()
+        # self.load_defaults()
 
     def load_defaults(self):
-        # stop when gripper is blocked
-        self.client.write_register(0x0505, 1, unit=self.slave_address)
-        # disable auto initialization
-        self.client.write_register(0x0504, 0, unit=self.slave_address)
+        # set up MODBUS control
+        self.client.write_register(0x1409, 2, unit=self.slave_address)
+        self.client.write_register(0x1108, 2, unit=self.slave_address)
+        # set initialization direction
+        self.client.write_register(0x0301, 1, unit=self.slave_address)
 
     def initialize(self, wait=True):
-        # Command: Initialize the gripper (0x0100 register, write A5)
-        response = self.client.write_register(0x0100, 0xA5, unit=self.slave_address)
-        self._check_response(response)
+        # write control word
+        # move to zero, enable robot
+        # 0100 0100 0000 0000
+        # run return to zero command
+        self.set_control_words(0x22)
+        time.sleep(0.2)
         start_time = time.time()
-        while self.check_initialization() != InitializationStatus.INITIALIZING and (
+        while self.read_motion_state() != RailStatus.MOVING and (
             time.time() - start_time < 5
         ):
             time.sleep(0.5)
         if wait:
-            while self.check_initialization() != InitializationStatus.INITIALIZED:
+            while self.read_motion_state() != RailStatus.ARRIVED:
                 time.sleep(0.5)
 
-    def save_configuration(self):
-        # save the configuration
-        response = self.client.write_register(0x0300, 1, unit=self.slave_address)
+    def get_control_words(self):
+        response = self.client.read_holding_registers(0x1605, unit=self.slave_address)
+        self._check_response(response)
+        return response.registers[0]
+
+    def set_control_words(self, command):
+        response = self.client.write_register(0x1605, command, unit=self.slave_address)
         self._check_response(response)
 
-    def check_initialization(self) -> InitializationStatus:
-        # Command: Check if the gripper is initialized (0x0100 register)
-        response = self.client.read_holding_registers(
-            0x0200, 1, unit=self.slave_address
-        )
+    def get_status(self):
+        response = self.client.read_holding_registers(0x1611, unit=self.slave_address)
         self._check_response(response)
-        return InitializationStatus(response.registers[0])
+        return response.registers[0]
 
-    def set_gripper_force(self, force_percentage):
-        # Command: Set the gripper force (0x0101 register, value 20-100%)
-        if 20 <= force_percentage <= 100:
-            response = self.client.write_register(
-                0x0101, force_percentage, unit=self.slave_address
-            )
-            self._check_response(response)
-        else:
-            raise ValueError("Force must be between 20 and 100 percent.")
-
-    def set_gripper_position(self, position_thousandths):
-        # Command: Set the gripper position (0x0103 register, value 0-1000‰)
-        if 0 <= position_thousandths <= 1000:
-            response = self.client.write_register(
-                0x0103, position_thousandths, unit=self.slave_address
-            )
-            self._check_response(response)
-        else:
-            raise ValueError("Position must be between 0 and 1000.")
-
-    def set_gripper_speed(self, speed_percentage):
-        # Command: Set the gripper speed (0x0104 register, value 1-100%)
-        if 1 <= speed_percentage <= 100:
-            response = self.client.write_register(
-                0x0104, speed_percentage, unit=self.slave_address
-            )
-            self._check_response(response)
-        else:
-            raise ValueError("Speed must be between 1 and 100 percent.")
-
-    def read_gripper_position(self):
-        # Command: Read the current position (0x0202 register)
-        response = self.client.read_holding_registers(
-            0x0202, 1, unit=self.slave_address
-        )
+    def read_motion_state(self) -> RailStatus:
+        response = self.client.read_holding_registers(0x1611, unit=self.slave_address)
         self._check_response(response)
-        return response.registers[0]  # Position value in thousandths (0-1000‰)
+        state = bin(response.registers[0])[2:]
+        return RailStatus(int(state[4]))
 
-    def read_gripper_status(self) -> GripperStatus:
-        # Command: Read gripper status (0x0201 register)
-        response = self.client.read_holding_registers(
-            0x0201, 1, unit=self.slave_address
-        )
-        self._check_response(response)
-        return GripperStatus(response.registers[0])
-
-    def set_rotation_speed(self, speed_percentage):
-        # Command: Set the rotation speed (0x0501 register, value 1-100%)
-        if 1 <= speed_percentage <= 100:
-            response = self.client.write_register(
-                0x0107, speed_percentage, unit=self.slave_address
-            )
-            self._check_response(response)
-        else:
-            raise ValueError("Speed must be between 1 and 100 percent.")
-
-    def set_rotation_force(self, force_percentage):
-        # Command: Set the rotation force (0x0108 register, value 1-100%)
-        if 20 <= force_percentage <= 100:
-            response = self.client.write_register(
-                0x0108, force_percentage, unit=self.slave_address
-            )
-            self._check_response(response)
-        else:
-            raise ValueError("Force must be between 20 and 100 percent.")
-
-    def set_rotation_angle(self, deg: int):
-        if -32768 <= deg <= 32767:
-            response = self.client.write_register(0x0109, deg, unit=self.slave_address)
-            self._check_response(response)
-        else:
-            raise ValueError("Angle must be between -32768 and 32767 degrees.")
-
-    def stop_rotation(self):
-        # Command: Stop the rotation (0x0502 register, write 1)
-        response = self.client.write_register(0x0502, 1, unit=self.slave_address)
-        self._check_response(response)
-
-    def read_rotation_status(self):
-        # Command: Read the rotation status (0x0503 register)
-        response = self.client.read_holding_registers(
-            0x020B, 1, unit=self.slave_address
-        )
-        self._check_response(response)
-        return RotationStatus(response.registers[0])
-
-    def rotate(
+    def move_to(
         self,
-        direction: RotationDirection,
-        deg: int = 720,
-        force: int = 30,
-        speed: int = 100,
-        check_gripper: bool = True,
-    ) -> RotationStatus:
-        if check_gripper and self.read_gripper_status() != GripperStatus.GRASPED:
-            raise ValueError("Gripper must be grasped before rotating.")
-        self.set_rotation_force(force)
-        self.set_rotation_speed(speed)
+        position,
+        max_speed: float = 250,
+        max_acceleration: float = 2000,
+        wait: bool = True,
+    ):
+        response = self.client.write_register(
+            0x1602, int(max_speed * 10), unit=self.slave_address
+        )
+        self._check_response(response)
 
-        try:
-            self.set_rotation_angle(direction.value * deg)
-            start_time = time.time()
-            while self.read_rotation_status() != RotationStatus.MOVING and (
-                time.time() - start_time < 5
-            ):
-                if (
-                    check_gripper
-                    and self.read_gripper_status() != GripperStatus.GRASPED
-                ):
-                    raise ValueError(
-                        f"Gripper status changed to {self.read_gripper_status()} during rotating"
-                    )
-                time.sleep(0.5)
+        response = self.client.write_register(
+            0x1603, int(max_acceleration), unit=self.slave_address
+        )
+        self._check_response(response)
 
-            while self.read_rotation_status() == RotationStatus.MOVING:
-                time.sleep(0.5)
-                if (
-                    check_gripper
-                    and self.read_gripper_status() != GripperStatus.GRASPED
-                ):
-                    raise ValueError(
-                        f"Gripper status changed to {self.read_gripper_status()} during rotating"
-                    )
+        response = self.client.write_register(
+            0x1600, int(position * 100), unit=self.slave_address
+        )
+        self._check_response(response)
+        # this is moving commands
+        self.set_control_words(0x21)
+        time.sleep(0.5)
+        if wait:
+            print(self.read_motion_state())
+            while self.read_motion_state() != RailStatus.ARRIVED:
+                print(1)
+                time.sleep(0.1)
+        self.set_control_words(0x20)
 
-            return self.read_rotation_status()
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            self.stop_rotation()
+    def read_alarm_code(self):
+        response = self.client.read_holding_registers(0x160F, unit=self.slave_address)
+        self._check_response(response)
+        return response.registers[0]
 
-    def grasp(self, speed_percentage=100, force_percentage=100):
-        self.set_gripper_speed(speed_percentage)
-        self.set_gripper_force(force_percentage)
-        self.set_gripper_position(0)
-
-        start_time = time.time()
-        while self.read_gripper_status() != GripperStatus.MOVING and (
-            time.time() - start_time < 1
-        ):
-            time.sleep(0.1)
-
-        while self.read_gripper_status() == GripperStatus.MOVING:
-            time.sleep(0.1)
-
-    def open_to(self, speed_percentage=100, force_percentage=100, position=1000):
-        self.set_gripper_speed(speed_percentage)
-        self.set_gripper_force(force_percentage)
-        self.set_gripper_position(position)
-
-        start_time = time.time()
-        while self.read_gripper_status() != GripperStatus.MOVING and (
-            time.time() - start_time < 1
-        ):
-            time.sleep(0.1)
-
-        while self.read_gripper_status() == GripperStatus.MOVING:
-            time.sleep(0.1)
+    def clear_alarm(self):
+        self.set_control_words(0x8)
 
     @staticmethod
     def _check_response(response):
@@ -250,22 +130,19 @@ class GripperController:
 # Example usage
 if __name__ == "__main__":
     # Initialize the gripper on COM port, assuming port name is 'COM3' or '/dev/ttyUSB0'
-    gripper = GripperController(
-        port="/dev/tty.usbserial-BG005IB3"
+    rail = LinearRailController(
+        port="/dev/tty.usbserial-BG004CS1"
     )  # Update the port based on your setup
-
-    try:
-        # Initialize the gripper
-        gripper.initialize()
-        gripper.save_configuration()
-        gripper.open_to(position=925)
-        # time.sleep(5)
-        # gripper.grasp()
-        gripper.rotate(
-            RotationDirection.CLOCKWISE, 18000, force=100, check_gripper=False
-        )
-        gripper.open_to(position=925)
-    except ModbusException as e:
-        print(f"An error occurred: {e}")
-    finally:
-        gripper.close()
+    # print(bin(rail.get_status())[2:][::-1])
+    # print(rail.clear_alarm())
+    for _ in range(50):
+        print(rail.move_to(0))
+        print(rail.move_to(50))
+    # rail.set_control_words(0x120)
+    # rail.set_control_words(0)
+    # time.sleep(5)
+    # rail.set_control_words(0x4)
+    # print(rail.initialize(wait=True))
+    # # print(rail.get_state())
+    # print(rail.read_alarm_code())
+    # print(rail.clear_alarm())
