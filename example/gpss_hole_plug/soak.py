25f9272c68ed854605ca6c1b0497ad05886aa2d3
Yuxing Fei <yuxingfei@berkeley.edu>
2025-04-08 18:18:52 -0700

    soak test hole plug

--- example/gpss_hole_plug/soak.py
+++ example/gpss_hole_plug/soak.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 from alab_control.dh_robotic_gripper.dh_robotic_gripper import GripperController
@@ -10,14 +11,40 @@ class Capper:
         self.gripper.initialize()
 
     def open(self):
+        self.open_fully()
+        time.sleep(0.2)
         self.gripper.open_to(position=500)
 
+    def open_fully(self):
+        self.gripper.open_to(position=1000)
+
     def close(self):
+        self.open_fully()
+        time.sleep(0.2)
         self.gripper.grasp()
 
 
-capper = Capper("COM13")
+capper = Capper("/dev/tty.usbserial-BG00U7A3")
 robot_arm = URRobotDashboard("192.168.1.24")
+
+
+def move_to_crucible_holder(pos):
+    robot_arm.run_program(
+        f"auto_program/pick_crucible_furnace_rack/pick_crucible_furnace_rack_{pos}.auto.urp"
+    )
+    capper.open_fully()
+    robot_arm.run_program("auto_program/place_cru_capper.auto.urp")
+    capper.close()
+    capper.open()
+    robot_arm.run_program("auto_program/pick_cru_capper.auto.urp")
+    robot_arm.run_program("auto_program/place_crucible_near_bdis.auto.urp")
+
+
+def put_back_crucible_holder(pos):
+    robot_arm.run_program("auto_program/pick_crucible_near_bdis.auto.urp")
+    robot_arm.run_program(
+        f"auto_program/place_crucible_furnace_rack/place_crucible_furnace_rack_{pos}.auto.urp"
+    )
 
 
 def load_crucible_to_capper():
@@ -51,18 +78,20 @@ def decapping(pos):
 
 
 if __name__ == "__main__":
-    # capper.open()
-    # exit()
     counter = 0
     SLOTS = ["A", "B", "C", "D"]
     while True:
-        for i in range(len(SLOTS)):
-            p = SLOTS[i]
-            p_n = SLOTS[i % len(SLOTS)]
+        for furnace_rack in range(1, 9):
             counter += 1
-            start = time.time()
-            print(f"Loop {counter}", end=" : ")
-            capping(p)
-            decapping(p_n)
-            end = time.time()
-            print(f"Time: {end - start} s")
+            slot_to_use = random.choice(SLOTS)
+            print(
+                f"Loop: {counter}: Furnace Rack position {furnace_rack}, Hole Plug slot {slot_to_use}",
+                end=" ",
+            )
+            start_time = time.time()
+            move_to_crucible_holder(furnace_rack)
+            decapping(slot_to_use)
+            capping(slot_to_use)
+            put_back_crucible_holder(furnace_rack)
+            end_time = time.time()
+            print(f"Time taken: {end_time - start_time:.2f} seconds")
