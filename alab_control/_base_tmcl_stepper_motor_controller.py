5e91f395895ea988426a7d2fe1fd696b8bafc9f6
Yuxing Fei <yuxingfei@berkeley.edu>
2025-04-16 21:41:13 -0700

    update

--- alab_control/_base_tmcl_stepper_motor_controller.py
+++ alab_control/_base_tmcl_stepper_motor_controller.py
@@ -5,6 +5,7 @@ from enum import IntEnum
 from struct import pack, unpack
 
 from serial import Serial
+from serial.tools.list_ports import comports
 
 
 class Commands(IntEnum):
@@ -58,10 +59,10 @@ class TMCLStepperMotorController:
     _has_external_encoder = False
     _VID = 0x2A3C
 
-    def __init__(self, port: str, _initialize: bool = True) -> None:
-        self.port = port
-        if _initialize:
-            self.update_motor_config()
+    def __init__(self, firmware_version: int) -> None:
+        self.firmware_version = firmware_version
+        self.port = self._determine_comport_by_firmware_version()
+        self.update_motor_config()
 
     def update_motor_config(self):
         raise NotImplementedError()
@@ -277,19 +278,14 @@ class TMCLStepperMotorController:
         )["value"]
         return version
 
-    @classmethod
-    def get_comport_by_firmware_version(cls, firmware_version: int) -> str | None:
-        from serial.tools.list_ports import comports
-
-        # find the ones made by TMCL by vid
+    def _determine_comport_by_firmware_version(self) -> str:
         tmcl_devices_port = [
-            com_port.device for com_port in comports() if com_port.vid == cls._VID
+            com_port for com_port in comports() if com_port.vid == self._VID
         ]
         for port in tmcl_devices_port:
-            _tmcl_device = TMCLStepperMotorController(port=port, _initialize=False)
-            if firmware_version == _tmcl_device.get_firmware_version():
-                return port
-        return None
+            if self.firmware_version == port.vid:
+                return port.device
+        raise RuntimeError(f"Firmware version {firmware_version} not found.")
 
     def __del__(self):
         # stop the motor when the object is deleted
