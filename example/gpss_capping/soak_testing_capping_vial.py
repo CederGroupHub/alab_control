3b58860b62b903f8064e228e9cd304c51b9489c5
Yuxing Fei <yuxingfei@berkeley.edu>
2025-06-07 18:42:11 -0700

    Add capping sequence to soak testing with capper integration

--- example/gpss_capping/soak_testing_capping_vial.py
+++ example/gpss_capping/soak_testing_capping_vial.py
@@ -1,28 +1,36 @@
 import time
 
 from alab_control.robot_arm_ur5e import URRobotDashboard
+from xmlrpc.client import ServerProxy
 
 robot_arm = URRobotDashboard("192.168.1.23")
+capper = ServerProxy("http://192.168.1.89:8872")
 
 
 def capping():
     robot_arm.run_program("auto_program/pick_cap_A.auto.urp")
     robot_arm.run_program("auto_program/capping.auto.urp")
+
+    capper.open_fully()
+    robot_arm.run_program("auto_program/place_vial_capper.auto.urp")
+    capper.open()
+    robot_arm.run_program("auto_program/pick_vial_capper.auto.urp")
+
     robot_arm.run_program("auto_program/decapping.auto.urp")
     robot_arm.run_program("auto_program/place_cap_A.auto.urp")
 
+    ##
+
     robot_arm.run_program("auto_program/pick_cap_B.auto.urp")
     robot_arm.run_program("auto_program/capping.auto.urp")
+
+    capper.open_fully()
+    robot_arm.run_program("auto_program/place_vial_capper.auto.urp")
+    capper.open()
+    robot_arm.run_program("auto_program/pick_vial_capper.auto.urp")
+
     robot_arm.run_program("auto_program/decapping.auto.urp")
     robot_arm.run_program("auto_program/place_cap_B.auto.urp")
-
-
-    # robot_arm.run_program("pick_trans_rack_vial.urp")
-    # robot_arm.run_program("place_cap_A.urp")
-    # robot_arm.run_program("pick_cap_B.urp")
-    # robot_arm.run_program("place_trans_rack_vial.urp")
-    # robot_arm.run_program("pick_cap_A.urp")
-    # robot_arm.run_program("place_trans_rack_cap_A.urp")
 
 
 if __name__ == "__main__":
