989cc80183001d7b43dc32399054dcf4813141c4
Bernardus Rendy <37468936+bernardusrendy@users.noreply.github.com>
2025-07-14 17:38:22 -0700

    Update xlim ender

--- alab_control/ender3/ender3.py
+++ alab_control/ender3/ender3.py
@@ -10,7 +10,7 @@ class Ender3(BaseGcodeRobot):
     TIMEOUT = 5  # timeout (in seconds) for waiting for a reply
     POSITIONTOLERANCE = 0.1  # tolerance (in mm) between the target and actual position. Positions which are close within this value will be considered equal.
     ZHOP_HEIGHT = 5  # height (in mm) to raise the z-axis between lateral movements. This is to avoid collisions.
-    XLIM = 200  # limit (in mm) of the x-axis
+    XLIM = 235  # limit (in mm) of the x-axis
     YLIM = 235  # limit (in mm) of the y-axis
     ZLIM = 150  # limit (in mm) of the z-axis
     MAX_XY_FEEDRATE = 10000  # maximum feedrate (in mm/min) of the x and y axes
